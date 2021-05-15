dffcb3955ec156c08b78369e22f95bcb05de0e3f
soumik21 <kundu.soumik008@gmail.com>
2021-05-15 19:38:26 +0530

    marge

---
+++ src/Methods.java
@@ -0,0 +1,11 @@
+
+public class Methods {
+
+	public String validateheader()
+	{
+		System.out.println("Header validated");
+		return "Pass";
+		
+	}
+
+}
---
+++ src/Parent.java
@@ -0,0 +1,15 @@
+
+public class Parent {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I started java programming");
+int a =2;
+int b =3;
+int sum = a+b;
+System.out.println(sum);
+Methods m = new Methods();
+System .out.println(m.validateheader());
+	}
+
+}
